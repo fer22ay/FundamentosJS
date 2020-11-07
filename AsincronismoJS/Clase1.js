91d21d20520335782a3065c90620fe29325e8951
Fernando Ambrosio <fer22ay@gmail.com>
2020-11-07 08:52:30 -0600

    Agregando nuevas plantaillas al proyecto

---
+++ AsincronismoJS/Clase1.js
@@ -0,0 +1,13 @@
+/**
+ * Memory Heap
+ * Event Loop --> cola de tareas (callback)
+ * Pila de ejecucion (call stack)
+ * 
+ * callback, promesas, ascyn, away
+ */
+
+ /**
+  * Que es un callback: Es una funcion que al crearla le pasamos como parametro una funcion.
+  */
+
+  
